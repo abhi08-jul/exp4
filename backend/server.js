9a373942a9c1426c450702ee8e1c1e6ed707dd85
abhi08-jul <soniabhinav230@gmail.com>
2026-08-19 11:49:05 +0530

    Update CORS configuration in server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_URL || "*" }));
+
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
